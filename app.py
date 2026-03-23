4f7a1dca22e67d21ce4fc472a02ccefa7ec3fc74
RizwanAhmed27 <rizwan@assist360.com.sg>
2026-03-23 11:50:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 
 from io import BytesIO
 from sklearn.ensemble import RandomForestRegressor, IsolationForest
